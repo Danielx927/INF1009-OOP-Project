ee37441e49136b938a6e01ab34654bd99d9ad0ee
Daniel Xie <2403412@sit.singaporetech.edu.sg>
2025-02-07 11:03:49 +0800

    first commit

---
+++ lwjgl3/src/main/java/game/gdx/lwjgl3/GameMaster.java
@@ -0,0 +1,20 @@
+package game.gdx.lwjgl3;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameMaster extends ApplicationAdapter{
+
+	@Override
+	public void create()
+	{
+		
+	}
+	
+	@Override
+	public void render()
+	{
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+	}
+}
