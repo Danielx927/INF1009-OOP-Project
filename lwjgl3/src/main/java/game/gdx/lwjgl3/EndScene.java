7a44da5c77215b177a5aa2cba8ea21f39bd8f74a
Heather <2402216@sit.singaporetech.edu.sg>
2025-02-18 21:55:33 +0800

    end scene

---
+++ lwjgl3/src/main/java/game/gdx/lwjgl3/EndScene.java
@@ -0,0 +1,13 @@
+package io.github.some_example_name.lwjgl3;
+
+import java.util.List;
+
+public class EndScene {
+	private List<Float> highScoreList;
+	private TextButton restartButton;
+	private TextButton quitButton;
+	
+	public void populateList() {
+		
+	}
+}
