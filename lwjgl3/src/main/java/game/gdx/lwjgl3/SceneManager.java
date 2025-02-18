5cb5680e3c849fb4e29ccbb43f5118a4f3facb4f
Heather <2402216@sit.singaporetech.edu.sg>
2025-02-18 16:01:47 +0800

    Update SceneManager.java

--- lwjgl3/src/main/java/game/gdx/lwjgl3/SceneManager.java
+++ lwjgl3/src/main/java/game/gdx/lwjgl3/SceneManager.java
@@ -5,9 +5,27 @@ import java.util.List;
 
 public class SceneManager {
 	private Scene currentScene;
-	private List<Scene> sceneList = new ArrayList<Scene>() ;
-
-	public void changeToScene(Scene scene) {
+	private List<Scene> sceneList = new ArrayList<Scene>();
+	private static SceneManager instance;
+	
+	public void changetoScene(Scene scene) {
+		if (currentScene != null) {
+			currentScene.unload();
+		}
+		
 		currentScene = scene;
+		currentScene.load();
+	}
+	
+	public Scene getCurrentScene() {
+		return currentScene;
+	}
+
+	public static SceneManager getInstance() {
+		// TODO Auto-generated method stub
+		if (instance == null) {
+			instance = new SceneManager();
+		}
+		return instance;
 	}
 }
