b8dcb356c2ed7b46c32adf17ce3946648c5e6602
Heather <2402216@sit.singaporetech.edu.sg>
2025-02-20 19:16:48 +0800

    added imports

--- lwjgl3/src/main/java/game/gdx/lwjgl3/EndScene.java
+++ lwjgl3/src/main/java/game/gdx/lwjgl3/EndScene.java
@@ -1,8 +1,21 @@
 package game.gdx.lwjgl3;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 
 public class EndScene extends Scene implements Screen {
 
