63f639de49d817bf9f0acddd485d4c8dbb04b662
Jaziel <jjaziel.c@gmail.com>
2025-03-24 23:41:10 +0800

    removed unnecessary imports

--- lwjgl3/src/main/java/game/gdx/lwjgl3/MainMenuScene.java
+++ lwjgl3/src/main/java/game/gdx/lwjgl3/MainMenuScene.java
@@ -2,13 +2,9 @@ package game.gdx.lwjgl3;
 
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
